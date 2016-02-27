f4cbe32e6ff909e86cd1904c6acf8dfd9e4402ac
Jorge Raze <jorge.raze@clevertech.biz>
2016-02-27 13:01:20 -0600

    Add bootstrap-material-design

--- user/js/main.js
+++ user/js/main.js
@@ -1,4 +1,6 @@
 // main.js
 window.$ = window.jQuery = require('jquery');
 var bootstrap = require('bootstrap');
+var bootstrap_material_design = require('bootstrap-material-design');
+$.material.init()
 var example = require('./login.js')
