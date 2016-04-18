682344772f99162462efab9f7bb224aa278b1b9d
Ireth <ireth@element.com.mx>
2016-04-18 15:22:52 -0500

    regex rfc validation

--- user/js/controller-addprofile.js
+++ user/js/controller-addprofile.js
@@ -1,5 +1,9 @@
 var app = angular.module("App");
 
 app.controller("addProfController", function($scope, $location, $auth, $http){
-
+    $scope.formData = [];
+    $scope.submit = function(){
+        console.log($scope.formData);
+        console.log('O_O');
+    };
 });
