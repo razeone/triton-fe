8f39537fada3446e4491d3b84837dd3c5acb8010
Ireth <ireth@element.com.mx>
2016-04-18 14:06:13 -0500

    submit test

--- user/js/controller-forgot.js
+++ user/js/controller-forgot.js
@@ -1,5 +1,7 @@
 var app = angular.module("App");
 
 app.controller("forgotController", function($scope, $location, $auth, $http){
-
+    $scope.rescue = function(){
+        console.log('pa que lo pinches pierdes, ahora te aguantas por pendejo');
+    };
 });
